f1b72c878272864fc9691afc51e0b434a29322e4
Eric <22939634+ericDevSantana@users.noreply.github.com>
2020-09-06 10:50:01 -0700

    Using JS Spread Operator

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,26 +10,33 @@ function App() {
   function handleChanges(event) {
     const { value, name } = event.target;
 
+    // setContact((prevValue) => {
+    //   if (name === "fName") {
+    //     return {
+    //       fName: value,
+    //       lName: prevValue.lName,
+    //       email: prevValue.email
+    //     };
+    //   } else if (name === "lName") {
+    //     return {
+    //       fName: prevValue.fName,
+    //       lName: value,
+    //       email: prevValue.email
+    //     };
+    //   } else if (name === "email") {
+    //     return {
+    //       fName: prevValue.fName,
+    //       lName: prevValue.lName,
+    //       email: value
+    //     };
+    //   }
+    // });
+
     setContact((prevValue) => {
-      if (name === "fName") {
-        return {
-          fName: value,
-          lName: prevValue.lName,
-          email: prevValue.email
-        };
-      } else if (name === "lName") {
-        return {
-          fName: prevValue.fName,
-          lName: value,
-          email: prevValue.email
-        };
-      } else if (name === "email") {
-        return {
-          fName: prevValue.fName,
-          lName: prevValue.lName,
-          email: value
-        };
-      }
+      return {
+        ...prevValue,
+        [name]: value
+      };
     });
   }
 
